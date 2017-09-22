c6c8ee341fa9721874c18d5027f4c2928335b597
LiudmilaTrafimuk <TrLuda@tut.by>
2017-09-22 19:29:33 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max + 1;
     }
 
     guess() {
-
+         return this.half = Math.floor((this.min + this.max) / 2);
     }
 
     lower() {
-
+        this.max = this.half + 1;
     }
 
     greater() {
-
+        this.min = this.half;
     }
 }
 
